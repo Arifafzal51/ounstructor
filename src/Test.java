f6a70e9329a844e0cba40eaf23ebd2339db7054c
arif afzal <arif.afzal_cs20@gla.ac.in>
2021-12-02 21:57:57 +0530

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,18 @@
+public class Test
+
+{
+    //int i;
+    //String s;
+    //Test()
+    {
+        System.out.println("no arg constructor");
+    }
+
+    public static void main(String[] args)
+    {
+        Test t = new Test();
+       // System.out.println(t.i+" "+t.s);
+
+    }
+
+}
